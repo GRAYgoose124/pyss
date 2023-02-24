cd85d85691bb355986b34e107e1c8157b4159789
Grayson Miller <grayson.miller124@gmail.com>
2023-02-23 18:28:51 -0700

    pieces drawing correctly

--- pyss/app/__main__.py
+++ pyss/app/__main__.py
@@ -13,7 +13,8 @@ class ChessApp(arcade.Window):
         self.offset = (self.width - self.board_size) // 2, (self.height - self.board_size) // 2
 
         self.play_board = None
-
+        self.board = None
+        
     def setup(self):
         self.play_board = Chessboard()
         self.create_board()
@@ -35,20 +36,28 @@ class ChessApp(arcade.Window):
 
         self.board = board
     
-    def create_piece(self, color, i, j):
-                # rotate visual i, j 90 degrees clockwise
-                ix, jx = j, 7 - i
-                
-                arcade.draw_text(self.play_board[i, j].unicode, 
-                                self.offset[0] + (ix * self.tile_size + self.tile_size * 0.5), 
-                                self.offset[1] + (jx * self.tile_size + self.tile_size * 0.5), 
-                                arcade.color.BLACK, font_size=self.tile_size // 2, anchor_x="center", anchor_y="center")
+    def draw_piece(self, i, j, rotate=True):
+        # rotate visual i, j 90 degrees clockwise
+        if not rotate:
+            ix, jx = i, j
+        else:
+            ix, jx = j, i
+        
+        if (i + j) % 2 == 0:
+            color = arcade.color.WHITE
+        else:
+            color = arcade.color.BLACK
+
+        arcade.draw_text(self.play_board[i, j].unicode, 
+                        self.offset[1] + (ix * self.tile_size + self.tile_size * .5), 
+                        self.offset[0] + (jx * self.tile_size + self.tile_size * .5), 
+                        color, font_size=self.tile_size // 2, anchor_x="center", anchor_y="center")
 
     def draw_pieces(self):
-        for j in range(8):
-            for i in range(8):
+        for i in range(8):
+            for j in range(8):
                 if self.play_board[i, j]:
-                    piece = self.create_piece(self.play_board[i, j].color, i, j)
+                    self.draw_piece(i, j)
 
     def on_draw(self):
         arcade.start_render()
@@ -56,15 +65,21 @@ class ChessApp(arcade.Window):
         self.draw_pieces()
 
     def update(self, delta_time):
-        if delta_time < 1/30:
+        if delta_time < 1/60:
             # randomly move a piece
             return
+
+        if self.play_board.updated:
+            print("Active pieces:", len(self.play_board.active_pieces))
+            self.play_board.consume_update = True
 
 
 def main():
     app = ChessApp()
     app.setup()
     arcade.run()
+
+
 
 
 if __name__ == "__main__":
--- pyss/game/board.py
+++ pyss/game/board.py
@@ -8,8 +8,13 @@ class Chessboard:
         - 2 players
     """
     def __init__(self):
+        self.reset()
+
+    def reset(self):
+        """Resets the board to its initial state"""
         self.board = Chessboard.initialize_board()
-        self._updated = None
+        self._active_pieces = None
+        self._updated = True
 
     @staticmethod
     def initialize_board():
@@ -19,16 +24,35 @@ class Chessboard:
             for color in piece_dict[piece]["initial_positions"]:
                 for position in piece_dict[piece]["initial_positions"][color]:
                     board[position[0]][position[1]] = Piece(color, piece)
-                    print(f"Initialized {color} {piece} at {position}")
         return board
 
-    def update(self):
+    @property
+    def updated(self):
         """Check if the board has updated since last call. """
         if self._updated:
             self._updated = False
             return True
         return False
     
+    @updated.setter
+    def consume_update(self, value):
+        """Consume the update flag"""
+        self._updated = False
+
+    @property
+    def active_pieces(self):
+        """Returns a list of all the pieces on the board"""
+        if self._active_pieces:
+            return self._active_pieces
+
+        pieces = []
+        for row in self.board:
+            for piece in row:
+                if piece:
+                    pieces.append(piece)
+
+        return pieces
+
     def move(self, piece, position):
         """Moves a piece to a new position"""
         self.board[piece.position[0]][piece.position[1]] = None
@@ -36,16 +60,9 @@ class Chessboard:
         piece.position = position
         self._updated = True
 
-    @property
-    def active_pieces(self):
-        """Returns a list of all the pieces on the board"""
-        pieces = []
-        for row in self.board:
-            for piece in row:
-                if piece:
-                    pieces.append(piece)
-        return pieces
-    
+    # position cmds
+    # TODO: parse position as tuple or notation
+    # TODO: parse move as tuple or notation
     def get_notation(self, position, move=None, capture=False):
         """Returns the notation of the piece at a given position"""
         piece = self.board[position[0]][position[1]]
