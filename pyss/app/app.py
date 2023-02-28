9abf38c2e0561c46ecdc1306ec0401da32ba2f1b
Grayson Miller <grayson.miller124@gmail.com>
2023-02-27 17:16:11 -0700

    Added basic self._theme to ChessApp.

--- pyss/app/app.py
+++ pyss/app/app.py
@@ -4,7 +4,7 @@ import arcade.gui
 import logging
 from pyss.app.draw_piece import load_pieces
 
-from pyss.app.utils import DEPTH_COLOR_PALETTE
+from pyss.app.utils import DEFAULT_THEME
 from ..game.board import Chessboard
 
 
@@ -49,6 +49,11 @@ class ChessApp(arcade.Window):
         self._selected_moves_list = None
 
         # ui
+        # self._theme = arcade.gui.Theme()
+            # theme
+        self._theme = DEFAULT_THEME
+
+            # manager
         self.v_manager = arcade.gui.UIManager()
         self.v_manager.enable()
         self.__create_gui()
@@ -178,9 +183,9 @@ class ChessApp(arcade.Window):
         for i in range(8):
             for j in range(8):                
                 if (i + j) % 2 == check:
-                    tile = create_tile(arcade.color.DARK_BROWN, i, j)
+                    tile = create_tile(self._theme['board']['dark_tile'], i, j)
                 else:
-                    tile = create_tile(arcade.color.LIGHT_BROWN, i, j)
+                    tile = create_tile(self._theme['board']['light_tile'], i, j)
 
                 board.append(tile)
 
@@ -205,19 +210,16 @@ class ChessApp(arcade.Window):
             drawlist.append(arcade.create_text_sprite(vertical, start_x=self.offset[0] - 20,
                                                       start_y=self.offset[1] + (
                                                           i * self.tile_size + self.tile_size * .5),
-                                                      color=arcade.color.YELLOW, font_size=14))
+                                                      color=self._theme['board']['rank_and_file_font_color'], font_size=self._theme['board']['rank_and_file_font_size']))
             drawlist.append(arcade.create_text_sprite(horizontal, start_x=self.offset[0] + (i * self.tile_size + self.tile_size * .5),
                                                       start_y=self.offset[1] +
                                                       self.board_size + 5,
-                                                      color=arcade.color.YELLOW, font_size=14))
+                                                      color=self._theme['board']['rank_and_file_font_color'], font_size=self._theme['board']['rank_and_file_font_size']))
         
         return drawlist
     
     def __draw_stats(self):
         """Draws the stats of the game."""
-        FONT_SIZE = 12
-        FONT_COLOR = arcade.color.RED
-
         # stats is middle right of the screen
         stats_offset = self.width - 120, self.height // 2
         box_size = (150, 340)
@@ -230,8 +232,8 @@ class ChessApp(arcade.Window):
             # draw at top of stat box which is centered on stats_offset
             text_offset = stats_offset[0] - \
                 box_size[0] // 4, stats_offset[1] + box_size[1] // 2 - 10
-            arcade.draw_text(f"Turn {self._turn_count}: {self.turn}", *text_offset, FONT_COLOR, FONT_SIZE, width=100, align="center",
-                             anchor_x="center", anchor_y="center")
+            arcade.draw_text(f"Turn {self._turn_count}: {self.turn}", *text_offset, self._theme['stats']['font_color'], self._theme['stats']['font_size'], width=100, align="center",
+                                anchor_x="center", anchor_y="center")
 
         # update score if turn has changed
         if self.turn != self._score_updated_on:
@@ -242,8 +244,7 @@ class ChessApp(arcade.Window):
         # draw score
         score_offset = stats_offset[0] - \
             box_size[0] // 4, stats_offset[1] - box_size[1] // 2 + 10
-        arcade.draw_text(f"Score: {self._score_updated}", *score_offset, FONT_COLOR, FONT_SIZE, width=100, align="center",
-                            anchor_x="center", anchor_y="center")
+        arcade.draw_text(f"Score: {self._score_updated}", *score_offset, self._theme['stats']['font_color'], self._theme['stats']['font_size'])
 
         # active pieces count
         # arcade.draw_text(f"White: {len(self.play_board._by_color['white'])}", 10, 50, FONT_COLOR, FONT_SIZE)
@@ -252,19 +253,19 @@ class ChessApp(arcade.Window):
         active_piece_offset = stats_offset[0] - \
             box_size[0] // 4, stats_offset[1] - box_size[1] // 2 + 30
         arcade.draw_text(
-            f"White: {len(self.play_board._by_color['white'])}", *active_piece_offset, FONT_COLOR, FONT_SIZE)
+            f"White: {len(self.play_board._by_color['white'])}", *active_piece_offset, self._theme['stats']['font_color'], self._theme['stats']['font_size'])
         arcade.draw_text(f"Black: {len(self.play_board._by_color['black'])}",
-                         active_piece_offset[0], active_piece_offset[1] + 20, FONT_COLOR, FONT_SIZE)
+                         active_piece_offset[0], active_piece_offset[1] + 20, self._theme['stats']['font_color'], self._theme['stats']['font_size'])
 
         # move history
         last_move_offset = stats_offset[0] - \
             box_size[0] // 3, stats_offset[1] + box_size[1] // 2 - 40
         arcade.draw_text(f"Last 10 Moves:", *last_move_offset,
-                         FONT_COLOR, FONT_SIZE + 2)
+                         self._theme['stats']['font_color'], self._theme['stats']['font_size'] + 2)
         for i, move in enumerate(self.play_board.move_history[-10:]):
             actual_turn = self.play_board.move_history.index(move) + 1
-            arcade.draw_text(f"  {actual_turn}:\t{move}", last_move_offset[0], last_move_offset[1] - (20 + i * 20), FONT_COLOR, FONT_SIZE)
-
+            arcade.draw_text(f"  {actual_turn}:\t{move}", last_move_offset[0], last_move_offset[1] - (20 + i * 20), self._theme['stats']['white_font_color' if actual_turn % 2 == 0 else 'black_font_color'], self._theme['stats']['font_size'])
+    
     def __draw_piece(self, i, j):
         """Draws the piece at the given position."""
         # rotate visual i, j 90 degrees clockwise
@@ -415,8 +416,8 @@ class ChessApp(arcade.Window):
 
         depth_drawlists = []
         for i, valid_moves in self._selected_depth_bins.items():
-            color = DEPTH_COLOR_PALETTE[(
-                self._depth_search - i) % len(DEPTH_COLOR_PALETTE)]
+            color = self._theme['depth']['color_palette'][(
+                self._depth_search - i) % len(self._theme['depth']['color_palette'])]
             drawlist = self.__create_moves_list(
                 valid_moves, color=color, size=(
                     self._depth_search - i) + 1)
--- pyss/app/utils.py
+++ pyss/app/utils.py
@@ -1,5 +1,6 @@
 import arcade.color as color
 
+
 DEPTH_COLOR_PALETTE = [color.ANDROID_GREEN,
                        color.AMBER,
                        color.AMETHYST,
@@ -7,3 +8,27 @@ DEPTH_COLOR_PALETTE = [color.ANDROID_GREEN,
                        color.BLUE_GRAY,
                        color.BLUE_SAPPHIRE,
                        color.RED]
+
+
+DEFAULT_THEME = {
+    "board": {
+        "light_tile": color.LIGHT_BROWN,
+        "dark_tile": color.DARK_BROWN,
+        "outline_color": color.BLACK,
+        "outline_width": 2,
+        "rank_and_file_font_color": color.PASTEL_ORANGE,
+        "rank_and_file_font_size": 16
+    },
+    "stats": {
+        "font_color": color.PASTEL_YELLOW,
+        "white_font_color": color.LAVENDER_GRAY,
+        "black_font_color": color.DARK_BLUE_GRAY,
+        "font_size": 12
+    },
+    "depth": {
+        "color_palette": DEPTH_COLOR_PALETTE
+    }
+}
+
+
+
